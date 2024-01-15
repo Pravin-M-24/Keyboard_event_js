a12b754286fc1b29f7fc7361ef9f4b8705e89079
Your Name <you@example.com>
2024-01-15 22:58:21 +0530

    audio added

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 
 let text = document.getElementById("text_container");
-
 let main = document.getElementById("main_container");
+const audio = new Audio("https://www.fesliyanstudios.com/play-mp3/387");
 
 let div = document.createElement("div");
       div.classList.add("newDiv");
 
 document.body.addEventListener("keydown" , (e)=>{
+          audio.play();
         let newKey = e.key;
         // newKey.classList.add("newKey");
        text.innerText =`You Pressed ${newKey}`;
